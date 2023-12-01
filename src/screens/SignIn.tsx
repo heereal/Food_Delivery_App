9890b2824eed84bf58ccb89c39920c2ecfc7685d
heeryoung <winterbooks@naver.com>
2023-12-01 16:32:04 +0900

    design: SignIn 화면 UI

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,11 +1,90 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import {
+  Alert,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = () => {
+    Alert.alert("알림", "로그인 되었습니다.");
+  };
+
+  const canGoNext = email && password;
+
   return (
-    <View>
-      <Text>로그인 화면</Text>
-    </View>
+    <>
+      <View style={styles.inputWrapper}>
+        <Text style={styles.label}>이메일</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="이메일을 입력해주세요."
+          onChangeText={setEmail}
+        />
+      </View>
+      <View style={styles.inputWrapper}>
+        <Text style={styles.label}>비밀번호</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="비밀번호를 입력해주세요."
+          onChangeText={setPassword}
+        />
+      </View>
+      <View style={styles.buttonZone}>
+        <Pressable
+          style={
+            canGoNext
+              ? StyleSheet.compose(styles.loginButton, styles.loginButtonActive)
+              : styles.loginButton
+          }
+          disabled={!canGoNext}
+          onPress={onSubmit}>
+          <Text style={styles.loginButtonText}>로그인</Text>
+        </Pressable>
+        <Pressable>
+          <Text>회원가입하기</Text>
+        </Pressable>
+      </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  textInput: {
+    padding: 5,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  inputWrapper: {
+    padding: 20,
+  },
+  label: {
+    fontWeight: "bold",
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  buttonZone: {
+    alignItems: "center",
+  },
+  loginButton: {
+    backgroundColor: "slateblue",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  loginButtonActive: {
+    backgroundColor: "blue",
+  },
+  loginButtonText: {
+    color: "white",
+    fontSize: 16,
+  },
+});
 
 export default SignIn;
