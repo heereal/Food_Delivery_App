e3b33424776af009b17fe32d40bfd9ec0e46552e
heeryoung <winterbooks@naver.com>
2023-12-02 15:16:49 +0900

    edit: DismissKeyboardView 컴포넌트 KeyboardAwareScrollView로 교체

--- src/components/DismissKeyboardView.tsx
+++ src/components/DismissKeyboardView.tsx
@@ -1,18 +1,12 @@
-import {
-  Keyboard,
-  KeyboardAvoidingView,
-  Platform,
-  TouchableWithoutFeedback,
-} from "react-native";
+import { PropsWithChildren } from "react";
+import { Keyboard, TouchableWithoutFeedback } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scrollview";
 
-// 키보드 상단 부분을 클릭했을 때 키보드를 닫기 위한 컴포넌트
-function DismissKeyboardView({ children }: { children: React.ReactNode }) {
+// 키보드 상단 부분을 클릭했을 때 키보드를 내리기 위한 컴포넌트
+function DismissKeyboardView({ children }: PropsWithChildren) {
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-      <KeyboardAvoidingView
-        behavior={Platform.OS === "android" ? "position" : "padding"}>
-        {children}
-      </KeyboardAvoidingView>
+      <KeyboardAwareScrollView>{children}</KeyboardAwareScrollView>
     </TouchableWithoutFeedback>
   );
 }
