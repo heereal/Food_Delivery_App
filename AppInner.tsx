02e76cc71704499d925c6a0f270e1ac0ce2937b5
heeryoung <winterbooks@naver.com>
2023-12-03 15:28:23 +0900

    feat: 앱 실행 시 토큰 있으면 자동로그인되는 로직 추가

--- AppInner.tsx
+++ AppInner.tsx
@@ -5,6 +5,13 @@ import { useSelector } from "react-redux";
 import { RootState } from "./src/store/reducer";
 import useSocket from "./src/hooks/useSocket";
 import { useEffect } from "react";
+import { useAppDispatch } from "./src/store";
+import orderSlice from "./src/slices/order";
+import EncryptedStorage from "react-native-encrypted-storage";
+import axios from "axios";
+import Config from "react-native-config";
+import userSlice from "./src/slices/user";
+import { Alert } from "react-native";
 
 // 로그인 여부에 따라 스크린을 제한하기 위해 type을 두 개로 나눔
 export type AuthenticatedParamList = {
@@ -23,34 +30,79 @@ const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator<UnauthenticatedParamList>();
 
 function AppInner() {
+  const dispatch = useAppDispatch();
   const isLoggedIn = useSelector((state: RootState) => !!state.user.email);
   const [socket, disconnect] = useSocket();
 
+  // 로그인 한 경우 웹소켓 연결
   useEffect(() => {
-    const helloCallback = (data: any) => {
+    const callback = (data: any) => {
       console.log(data);
+      dispatch(orderSlice.actions.addOrder(data));
     };
 
     if (socket && isLoggedIn) {
-      console.log(socket);
-      socket.emit("login", "hello"); // 서버에 데이터 전송
-      socket.on("hello", helloCallback); // 서버로부터 데이터 수신
+      socket.emit("acceptOrder", "hello"); // 서버에 데이터 전송
+      socket.on("order", callback); // 서버로부터 데이터 수신
     }
 
     // cleanup
     return () => {
       if (socket) {
-        socket.off("hello", helloCallback);
+        socket.off("order", callback);
       }
     };
-  }, [isLoggedIn, socket]);
+  }, [dispatch, isLoggedIn, socket]);
 
+  // 로그인 안한 경우 웹소켓 연결 끊기
   useEffect(() => {
     if (!isLoggedIn) {
       console.log("!isLoggedIn", !isLoggedIn);
       disconnect();
     }
   }, [isLoggedIn, disconnect]);
+
+  // 앱 실행 시 토큰 있으면 로그인함
+  useEffect(() => {
+    const getTokenAndRefresh = async () => {
+      try {
+        const token = await EncryptedStorage.getItem("refreshToken");
+        if (!token) {
+          return;
+        }
+
+        const response = await axios.post(
+          `${Config.API_URL}/refreshToken`,
+          {},
+          {
+            headers: {
+              authorization: `Bearer ${token}`,
+            },
+          },
+        );
+
+        dispatch(
+          userSlice.actions.setUser({
+            name: response.data.data.name,
+            email: response.data.data.email,
+            accessToken: response.data.data.accessToken,
+          }),
+        );
+      } catch (error) {
+        console.error(error);
+        // refreshToken이 만료되었을 경우
+        if (
+          axios.isAxiosError(error) &&
+          error.response?.data.code === "expired"
+        ) {
+          Alert.alert("알림", "다시 로그인 해주세요.");
+        }
+      } finally {
+        // 잠깐 로그인 화면이 보이는 것은 SplashScreen으로 숨길 예정
+      }
+    };
+    getTokenAndRefresh();
+  }, [dispatch]);
 
   return isLoggedIn ? (
     <Tab.Navigator>
