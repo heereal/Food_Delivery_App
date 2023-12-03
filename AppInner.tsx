59753e8ab4be73d6d00dd28f8c0422859ef3fe19
heeryoung <winterbooks@naver.com>
2023-12-03 14:25:54 +0900

    add: AppInner에 웹소켓 연결 로직 추가

--- AppInner.tsx
+++ AppInner.tsx
@@ -3,6 +3,8 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Delivery, Orders, Settings, SignIn, SignUp } from "./src/screens";
 import { useSelector } from "react-redux";
 import { RootState } from "./src/store/reducer";
+import useSocket from "./src/hooks/useSocket";
+import { useEffect } from "react";
 
 // 로그인 여부에 따라 스크린을 제한하기 위해 type을 두 개로 나눔
 export type AuthenticatedParamList = {
@@ -22,6 +24,33 @@ const Stack = createNativeStackNavigator<UnauthenticatedParamList>();
 
 function AppInner() {
   const isLoggedIn = useSelector((state: RootState) => !!state.user.email);
+  const [socket, disconnect] = useSocket();
+
+  useEffect(() => {
+    const helloCallback = (data: any) => {
+      console.log(data);
+    };
+
+    if (socket && isLoggedIn) {
+      console.log(socket);
+      socket.emit("login", "hello"); // 서버에 데이터 전송
+      socket.on("hello", helloCallback); // 서버로부터 데이터 수신
+    }
+
+    // cleanup
+    return () => {
+      if (socket) {
+        socket.off("hello", helloCallback);
+      }
+    };
+  }, [isLoggedIn, socket]);
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      console.log("!isLoggedIn", !isLoggedIn);
+      disconnect();
+    }
+  }, [isLoggedIn, disconnect]);
 
   return isLoggedIn ? (
     <Tab.Navigator>
