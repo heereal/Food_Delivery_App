241fbd4bb04caaede6fda65cb6eff991ea78f2b2
heeryoung <winterbooks@naver.com>
2023-12-05 13:53:30 +0900

    add: 네이버 지도 추가 설정 및 렌더링

--- src/components/EachOrder.tsx
+++ src/components/EachOrder.tsx
@@ -1,4 +1,11 @@
-import { Alert, Pressable, StyleSheet, Text, View } from "react-native";
+import {
+  Alert,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+} from "react-native";
 import React, { useCallback, useState } from "react";
 import orderSlice, { Order } from "../slices/order";
 import { useAppDispatch } from "../store";
@@ -9,6 +16,7 @@ import Config from "react-native-config";
 import { NavigationProp, useNavigation } from "@react-navigation/native";
 import { AuthenticatedParamList } from "../../AppInner";
 import getDistanceFromLatLonInKm from "../utils/getDistanceFromLatLonInKm";
+import NaverMapView, { Marker, Path } from "react-native-nmap";
 
 interface Props {
   item: Order;
@@ -18,6 +26,7 @@ function EachOrder({ item }: Props) {
   const navigation = useNavigation<NavigationProp<AuthenticatedParamList>>();
   const dispatch = useAppDispatch();
 
+  const { start, end } = item;
   const accessToken = useSelector((state: RootState) => state.user.accessToken);
   const [detail, showDetail] = useState(false);
 
@@ -57,8 +66,6 @@ function EachOrder({ item }: Props) {
     showDetail(prevState => !prevState);
   };
 
-  const { start, end } = item;
-
   return (
     <View style={styles.orderContainer}>
       <Pressable onPress={toggleDetail} style={styles.info}>
@@ -78,7 +85,47 @@ function EachOrder({ item }: Props) {
       {detail && (
         <View>
           <View>
-            <Text>네이버맵이 들어갈 장소</Text>
+            <View
+              style={{
+                width: Dimensions.get("window").width - 30,
+                height: 200,
+                marginTop: 10,
+              }}>
+              <NaverMapView
+                style={{ width: "100%", height: "100%" }}
+                zoomControl={false}
+                center={{
+                  zoom: 10,
+                  tilt: 100,
+                  latitude: (start.latitude + end.latitude) / 2,
+                  longitude: (start.longitude + end.longitude) / 2,
+                }}>
+                {/* 출발지 */}
+                <Marker
+                  coordinate={{
+                    latitude: start.latitude,
+                    longitude: start.longitude,
+                  }}
+                  pinColor="blue"
+                />
+                <Path
+                  coordinates={[
+                    {
+                      latitude: start.latitude,
+                      longitude: start.longitude,
+                    },
+                    { latitude: end.latitude, longitude: end.longitude },
+                  ]}
+                />
+                {/* 도착지 */}
+                <Marker
+                  coordinate={{
+                    latitude: end.latitude,
+                    longitude: end.longitude,
+                  }}
+                />
+              </NaverMapView>
+            </View>
           </View>
           <View style={styles.buttonWrapper}>
             <Pressable onPress={onAccept} style={styles.acceptButton}>
@@ -103,6 +150,7 @@ const styles = StyleSheet.create({
   },
   info: {
     flexDirection: "row",
+    justifyContent: "center",
   },
   eachInfo: {
     flex: 1,
